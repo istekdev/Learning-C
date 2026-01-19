ac64fd6762149f4f3a7ddf734c6ffd439b45ec17
ISTEK <200693214+istekdev@users.noreply.github.com>
2026-01-18 23:43:58 -0600

    Create day1.c

---
+++ day1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  int numero = 0;
+  while (numero < 100000) {
+    numero += 1;
+  }
+  printf("I finally got to %d. See? I'm better than Python.\n", numero);
+  return 0;
+
+/* Day 1 of C Programming
+This program counts to 100,000 and alerts me afterward.
+Uses: include, int, while, return, printf
+Comment: Similar to Python, easy yet insanely confusing. Looking forward to Day 2
+*/
