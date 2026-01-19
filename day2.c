4eb19349f9d573dda07b075d91ad44132e5eb19f
ISTEK <200693214+istekdev@users.noreply.github.com>
2026-01-19 15:59:09 -0600

    Update day2.c

--- day2.c
+++ day2.c
@@ -2,7 +2,7 @@
 #include <time.h> // An overdramatic version of "import time"
 
 int main() {
-const int limit = 1000000;
+const int limit = 1000000; // Can literally be any number, just remember to add long when it gets too big. "int" is for 10 digit numbers, "long" is for 19 digit numbers, and "long long" is for 19 digits and above.
 int largenum = 0;
 int difference;
 time_t start;
