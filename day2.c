48dc88c7f485c26fc5a94d63f28bb23351cad8c7
ISTEK <200693214+istekdev@users.noreply.github.com>
2026-01-19 14:12:31 -0600

    Create day2.c

---
+++ day2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h> // An overdramatic version of "import time"
+
+int main() {
+const int limit = 1000000;
+int largenum = 0;
+int difference;
+time_t start;
+time_t end;
+start = time(NULL); // NULL sets it to the time from now to the UNIX epoch.
+
+while (largenum < limit) {
+largenum++; // Turns out, this is more minimalistic compared to +=
+  }
+end = time(NULL);
+difference = end - start;
+printf("Task Finished!\nLimit: %i\nTime: %i Seconds\n", largenum, difference);
+return 0;
+}
+
+/* Day 2 of C Programming
+This program is a modification of Day 1, utilizing the UNIX timestamp to measure how long it takes to count to 1000000.
+Uses: const, include, int, time, while, printf, and return
+Comment: I ran out of ideas, so I decided to try and work my way to the simpler modules, and this is insanely fast. Python would catch on fire if it was given this exact instruction. Good job, C. */
